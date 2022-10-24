bdf8916cc6c3c2fbbdb897ff7359e17077d011f4
NontapatJeans <115334386+NontapatJeans@users.noreply.github.com>
2022-10-25 06:13:13 +0700

    Add files via upload

---
+++ Player movement assets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+
+    public float speed = 5f;
+    Rigidbody myRigidbody;
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+       Vector3 movement = new Vector3(moveHorizontal, 0f,moveVertical);
+
+        myRigidbody.AddForce(movement * speed);
+	}
+}
